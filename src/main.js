7034237a5775ef811b26141058dd071b5e3087c3
softimis <2365961568@qq.com>
2019-06-20 23:01:56 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -3,11 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import qs from 'qs'
+Vue.prototype.$qs = qs;
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
+Vue.prototype.axios = axios;
 
+axios.defaults.baseURL = 'http://localhost:8099/imis'
+//配置请求头
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
+  <div class="main_body">
     <h2>登录页</h2>
+    <form >
+    <el-input class="login_input" type="text" v-model="username" placeholder="请输入用户名"></el-input>
+    <el-input class="login_input" type="password" v-model="password" placeholder="请输入密码"></el-input>
+    <el-button class="login_btn" type="primary" @click="login">登录</el-button>
+    </form>
   </div>
 </template>
 
 <script>
     export default {
-        name: "login"
+        name: "login",
+        data(){
+          return{
+            username:'',
+            password:''
+          }
+        },
+      methods:{
+        login: function () {
+          const data={'username':this.username,'password':this.password}
+          this.axios.post('/testController/hello',this.$qs.stringify(data))
+          .then(function (res) {
+            console.log(res)
+          }).catch(function (res) {
+            console.log(res)
+          })
+        }
+      }
     }
 </script>
 
 <style scoped>
-
+.main_body{
+  text-align: center;
+}
+.login_input{
+  width: 300px;
+  height: 100%;
+  display: block;
+  margin: 10px auto;
+}
+.login_btn{
+  width: 300px;
+  height: 100%;
+}
 </style>
